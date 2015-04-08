31434a5d04ea0713ed5fb976e8254b6ae1d9f25f
Alexey <alexey.nikituyk@gmail.com>
2015-04-08 18:13:43 +0300

    Sides of rectangle.

---
+++ Hillel/src/Rectangle.java
@@ -0,0 +1,26 @@
+
+public class Rectangle {
+    public static void main(String[] params){
+         double perimeter = 24;
+         double area = 36;
+         findoutsides(perimeter, area);
+    }
+    public static void findoutsides(double perimeter, double area){
+        //eitherside^2 - eitherside*perimeter/2 + area=0
+        double width;
+        double length;
+        double discriminant = Math.pow((perimeter / 2),2) - 4*area;
+        if (discriminant < 0) {
+            System.out.println("There is no possible solution");
+        } else if(discriminant==0){
+            width = length = (perimeter / 2) / 2;
+            System.out.println("Width = " + width);
+            System.out.println("Length = " + length);
+        } else if(discriminant>0) {
+            width = ((perimeter / 2) + Math.sqrt(discriminant)) / 2;
+            length = ((perimeter / 2) - Math.sqrt(discriminant)) / 2;
+            System.out.println("Width = " + width);
+            System.out.println("Length = " + length);
+        }
+    }
+}
