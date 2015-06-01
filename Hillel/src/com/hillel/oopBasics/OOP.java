9c5ecfe0e8f76430c3f548fc8e91431a69d0defc
Alexey <alexey.nikituyk@gmail.com>
2015-06-02 01:07:38 +0300

    Merge branch 'master' of A:\Projects\IntelliJ IDEA Projects\hilleljava with conflicts.

--- Hillel/src/com/hillel/oopBasics/OOP.java
+++ Hillel/src/com/hillel/oopBasics/OOP.java
@@ -1,5 +1,3 @@
-package com.hillel.oopBasics;
-
 /**
  * Created by A.Nikitiuk on 10.04.2015.
  */
@@ -7,32 +5,23 @@ public class OOP {
 
     public static void main(String[] args) {
         oopIntro();
-        //negativeAge();
-        Cat cat1 = new Cat("Tom", 2);
+        /*Cat cat1 = new Cat("Tom", 2);
         Cat cat2 = new Cat("Tom", 2);
         System.out.println("They are equal using ==: " + (cat1 == cat2));
-        System.out.println("They are equal using equals(): " + cat1.equals(cat2));
+        System.out.println("They are equal using equals(): " + cat1.equals(cat2));*/
     }
 
     public static void oopIntro() {
-        Cat.pawCount();
-        System.out.println(Cat.pawCount());
+        /*Cat.pawCount();
+        System.out.println(Cat.pawCount());*/
         Cat cat1 = new Cat("Tom", 2);
         System.out.println("default to string: " + cat1);
         Cat cat2 = new Cat("Murzik", 1);
         Vet vet = new Vet();
-        Cat cat3 = null;
         vet.registerCat(cat1);
         vet.registerCat(cat2);
-        vet.registerCat(cat3);
-        vet.print();
         vet.makeVaccine();
         vet.print();
     }
 
-    public static void negativeAge(){
-        Cat cat = new Cat();
-        cat.setAge(-1);
-        System.out.println("this mesage never be printed");
-    }
 }
