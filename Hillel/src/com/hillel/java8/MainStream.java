cdea1df07c650fb7f6ca2155f2242675fb0b7f42
ANikitiuk <rake.dragnipur@gmail.com>
2015-07-27 21:07:45 +0300

    java8 X3 + Final

--- Hillel/src/com/hillel/java8/MainStream.java
+++ Hillel/src/com/hillel/java8/MainStream.java
@@ -1,16 +1,23 @@
 package com.hillel.java8;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
+ * Stream examples
  * Created by ANikitiuk on 23.07.2015.
+ *
  */
-public class MainStream {
+public class MainStream {//ctrl+q
+//Хорстман + тдд + рефакторинг + начальные книги + философия джава + алгоритмы(НЕ Кнут) Сэджвик + НЕ(!) Шилдт + изучать фреймворки(например Хибернэйт, Спринг) + GWT(k) + коллекции(это важно) + многопоточности(concurency in practice)
+//
+    /**
+     * this is main method
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         List<Apple> apples = new ArrayList<>();
 
@@ -29,14 +36,54 @@ public class MainStream {
                 .collect(Collectors.toList());
 
         Optional<String> maxColor = apples.parallelStream()
-                .filter(apple -> apple.getWeight() > 120)
+                .filter(apple -> apple.getWeight() > 120) //здесь лямбда реализует интерфейс предикат(принимает объект, возвращает булеан)
                 .limit(2)
                 .map(Apple::getColor) //.map(appleToString)
                 .sorted()
                 .max(Comparator.naturalOrder());
 
+  /*      Random random = new Random();
+        int result = Stream.generate(random::nextInt)
+                .limit(10)
+                .mapToInt(value -> value)
+                .min()
+                .orElse(0);
+        System.out.println(result);
 
+        System.out.println(maxColor.orElse("none"));*/
 
-        System.out.println(maxColor.orElse("none"));
+        System.out.println( "contains all green apples " +
+                        apples.stream()
+                                .allMatch(apple -> apple.getColor().equals("Green"))
+        );
+
+        System.out.println("contains at least one green apple " +
+                        apples.stream()
+                                .anyMatch(apple -> apple.getColor().equals("Green"))
+        );
+
+        Predicate<Apple> isGreen = apple -> apple.getColor().equals("Green");
+        Predicate<Apple> isYellow = apple -> apple.getColor().equals("Yellow");
+        Predicate<Apple> isYellowOrGreen = isGreen.or(isYellow);
+        System.out.println( "contains only green or yellow apples " +
+                        apples.stream()
+                                .allMatch(isYellowOrGreen)
+        );
+
+        apples.stream().forEach(System.out::println);
     }
+
+    /**
+     *
+     * @param val input value
+     * @return always false
+     */
+    public static boolean someMethod(int val){
+
+        /*Class classObject =  Object.class.getClassLoader().loadClass("");
+        Apple apple = (Apple) classObject.newInstance();
+        apple.getClass().getMethods()*/
+        return false;
+    }
+
 }
